88d716b2818a62409ea2ce4a33fc7ed033b04f2c
oliverpauffley <mrpauffley@gmail.com>
2024-08-18 21:19:08 +0100

    feat: semvar parser

--- src/Parse.hs
+++ src/Parse.hs
@@ -1,8 +1,6 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 module Parse where
-import           Text.Parsec            (alphaNum, char, digit, many, parse,
-                                         skipMany, (<|>))
+import           Text.Parsec            (alphaNum, char, digit, endOfLine, many,
+                                         parse, skipMany, (<|>))
 import           Text.Parsec.Combinator
 import           Text.Parsec.String     (Parser)
 
@@ -12,7 +10,8 @@ data NumberOrString =
   deriving (Eq, Show)
 
 instance Ord NumberOrString where
-  compare (NOSS _ ) (NOSI _) = GT
+  compare (NOSS a ) (NOSI b) = GT
+  compare (NOSI a ) (NOSS b) = LT
   compare (NOSI a ) (NOSI b) = compare a b
   compare (NOSS a ) (NOSS b) = compare a b
 
@@ -23,7 +22,8 @@ newtype Fields = Fields [NumberOrString]
 
 instance Ord Fields where
   compare (Fields []) (Fields [])    =  EQ
-  compare (Fields []) (Fields b)     = GT
+  compare (Fields []) (Fields _)     = GT
+  compare (Fields _) (Fields [])     = LT
   compare (Fields (x:xs)) (Fields (y:ys)) = case compare x y of
     EQ -> compare xs ys
     GT -> GT
@@ -40,12 +40,16 @@ data SemVer =
   deriving (Eq, Show)
 
 instance Ord SemVer where
-  compare (SemVer maj1 min1 pat1 rel1 met1) (SemVer maj2 min2 pat2 rel2 met2)
+  compare (SemVer maj1 min1 pat1 rel1 _meta1) (SemVer maj2 min2 pat2 rel2 _meta2)
     | maj1 > maj2 = GT
+    | maj1 < maj2 = LT
     | min1 > min2 = GT
+    | min1 < min2 = LT
     | pat1 > pat2 = GT
+    | pat1 < pat2 = LT
     | rel1 > rel2 = GT
-    | met1 > met2 = GT
+    | rel1 < rel2 = LT
+    | otherwise = EQ
 
 parseNoS :: Parser NumberOrString
 parseNoS = do
@@ -62,22 +66,13 @@ parseSemVer = do
   char '.'
   patch <- read <$> many1 digit
 
+
   optional $ char '-'
   release <- parseNoSs
 
   optional $ char '+'
   meta <- parseNoSs
 
+  eof
+
   return $ SemVer major minor patch release meta
-
-testBasic = "10.1.1"
-
-testRelease =  "10.1.1-alpha.1"
-testReleaseAndMeta =  "10.1.1-alpha.1+mac"
-testMeta = "10.1.1+linux.1.0.0"
-
-test = do
-  print $ parse parseSemVer "" testBasic
-  print $ parse parseSemVer "" testRelease
-  print $ parse parseSemVer "" testMeta
-  print $ parse parseSemVer "" testReleaseAndMeta
--- test/Main.hs
+++ test/Main.hs
@@ -7,20 +7,51 @@ import           Text.Parsec                     (parse)
 
 main :: IO ()
 main = hspec $ do
-  it "can parse simple semantic version" $ do
-    parse parseSemVer "" "1.3.4" `shouldBe` Right (SemVer 1 3 4 (Fields []) (Fields []))
+  describe "Parsing semantic versions" $ do
+    it "can parse simple semantic version" $ do
+      parse parseSemVer "" "1.3.4" `shouldBe` Right (SemVer 1 3 4 (Fields []) (Fields []))
 
-  it "can parse a semantic version with release" $ do
-    parse parseSemVer "" "1.3.4-alpha.1" `shouldBe` Right (SemVer 1 3 4 (Fields [NOSS "alpha", NOSI 1]) (Fields []))
+    it "can parse a semantic version with release" $ do
+      parse parseSemVer "" "1.3.4-alpha.1" `shouldBe` Right (SemVer 1 3 4 (Fields [NOSS "alpha", NOSI 1]) (Fields []))
 
-  it "can parse a semantic version with metadata" $ do
-    parse parseSemVer "" "1.3.4+linux.1.2" `shouldBe` Right (SemVer 1 3 4 (Fields []) (Fields [NOSS "linux", NOSI 1, NOSI 2]))
+    it "can parse a semantic version with metadata" $ do
+      parse parseSemVer "" "1.3.4+linux.1.2" `shouldBe` Right (SemVer 1 3 4 (Fields []) (Fields [NOSS "linux", NOSI 1, NOSI 2]))
 
-  it "can parse a semantic version with release and metadata" $ do
-    parse parseSemVer "" "1.3.4-alpha.1+linux.1.2" `shouldBe` Right (SemVer 1 3 4 (Fields [NOSS "alpha", NOSI 1]) (Fields [NOSS "linux", NOSI 1, NOSI 2]))
+    it "can parse a semantic version with release and metadata" $ do
+      parse parseSemVer "" "1.3.4-alpha.1+linux.1.2" `shouldBe` Right (SemVer 1 3 4 (Fields [NOSS "alpha", NOSI 1]) (Fields [NOSS "linux", NOSI 1, NOSI 2]))
 
-  it "incorrect semantic versions don't parse" $ do
-    parse parseSemVer "" "1.three.4-alpha.1+linux.1.2" `shouldSatisfy` isLeft
-    parse parseSemVer "" "1.3.4+alpha.1+linux.1.2" `shouldSatisfy` isLeft
-    parse parseSemVer "" "1.3.4-alpha.1-linux.1.2" `shouldSatisfy` isLeft
-    parse parseSemVer "" "1.3.4.4" `shouldSatisfy` isLeft
+    it "incorrect semantic versions don't parse" $ do
+      parse parseSemVer "" "1.three.4-alpha.1+linux.1.2" `shouldSatisfy` isLeft
+      parse parseSemVer "" "1.3.4+alpha.1+linux.1.2" `shouldSatisfy` isLeft
+      parse parseSemVer "" "1.3.4-alpha.1-linux.1.2" `shouldSatisfy` isLeft
+      parse parseSemVer "" "1.3.4.4" `shouldSatisfy` isLeft
+
+  describe "Ordering semantic versions" $ do
+    it "can order major minor patch semantic versions" $ do
+      compare (p "1.0.3") (p "1.3.2") `shouldBe` LT
+      compare (p "1.3.2") (p "1.3.2") `shouldBe` EQ
+      compare (p "1.3.4") (p "1.3.2") `shouldBe` GT
+
+    it "can order major minor patch semantic versions with releases" $ do
+      compare (p "1.3.2-alpha") (p "1.3.2") `shouldBe` LT
+      compare (p "1.3.2-alpha") (p "1.3.2-alpha") `shouldBe` EQ
+      compare (p "1.3.2-alpha") (p "1.3.2-beta") `shouldBe` LT
+      compare (p "1.3.2-alpha.beta") (p "1.3.2-beta") `shouldBe` LT
+      compare (p "1.3.2-beta") (p "1.3.2-beta.2") `shouldBe` LT
+      compare (p "1.3.2-beta.2") (p "1.3.2-beta.11") `shouldBe` LT
+      compare (p "1.3.2-2") (p "1.3.2-beta") `shouldBe` LT
+      compare (p "1.3.2-2.1") (p "1.3.2-2.0") `shouldBe` GT
+      compare (p "1.3.2-2.1") (p "1.3.2-2") `shouldBe` GT
+      compare (p "1.3.2-2.1") (p "1.3.2-2.1.1") `shouldBe` LT
+
+    it "metadata is ignored for ordering" $ do
+      compare (p "1.3.2+alpha") (p "1.3.2") `shouldBe` EQ
+      compare (p "1.3.1+alpha") (p "1.3.2") `shouldBe` LT
+      compare (p "2.3.1+alpha") (p "1.3.2") `shouldBe` GT
+
+p :: String -> SemVer
+p s = unpack parsePlease
+  where
+    parsePlease = parse parseSemVer "" s
+    unpack (Right result) = result
+    unpack _              = error "could not unpack"
